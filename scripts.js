27885fea23d04b092c08632946d967687e5c36e0
Muhammad Zaki Kurniawan <kurniawan.zaki279@gmail.com>
2022-08-26 22:03:50 +0700

    add coba-coba code

--- scripts.js
+++ scripts.js
@@ -1,44 +1,57 @@
-// belajar var
-var diskon,nama;
-nama = "aku";
-nama = "siapa?"
-diskon = 500;
-if(diskon == true){
-    diskon = 300;
-}
-console.log(nama);
-console.log(diskon); //outputnya 500 karena variabel diskon bersifat global
+// // belajar var
+// var diskon,nama;
+// nama = "aku";
+// nama = "siapa?"
+// diskon = 500;
+// if(diskon == true){
+//     diskon = 300;
+// }
+// console.log(nama);
+// console.log(diskon); //outputnya 500 karena variabel diskon bersifat global
 
-function scopeDiskon(){
-    var diskon = 300;
-    console.log(diskon)// outputnya 300 karena variabel diskon bersifat lokal
-}
-scopeDiskon()
-// end belajar var
-// button
-function myFunction(){
-    document.getElementById("ubah").innerHTML ="aku zaki";
-}
-// end button
-// belajar let
-let potongan;
-potongan = 100;
-if(true){
-    potongan = 400;
-    console.log(potongan); //outputnya 400 karena let diskon bersifat lokal
-}
-// end belajar let
-// belajar const
-const WARNA_MERAH = "#F00";
-const WARNA_BIRU = "#00F";
-const WARNA_HIJAU = "#0F0";
+// function scopeDiskon(){
+//     var diskon = 300;
+//     console.log(diskon)// outputnya 300 karena variabel diskon bersifat lokal
+// }
+// scopeDiskon()
+// // end belajar var
+// // button
+// function myFunction(){
+//     document.getElementById("ubah").innerHTML ="aku zaki";
+// }
+// // end button
+// // belajar let
+// let potongan;
+// potongan = 100;
+// if(true){
+//     potongan = 400;
+//     console.log(potongan); //outputnya 400 karena let diskon bersifat lokal
+// }
+// // end belajar let
+// // belajar const
+// const WARNA_MERAH = "#F00";
+// const WARNA_BIRU = "#00F";
+// const WARNA_HIJAU = "#0F0";
 
-let warnaBaju = WARNA_BIRU;
-if(true){
-    console.log(warnaBaju);
-}
-const stringIsiKutipSatu = 'ini string tapi aku bisa nulis kutip dua disini, liat nih ", tuh kan !'
-console.log(stringIsiKutipSatu);
-const namaDepan = "Zaki";
-const tempatLahir = "Tenggarong";
-console.log(`Hallo, Nama ku ${namaDepan}, aku lahir di ${tempatLahir}`);
+// let warnaBaju = WARNA_BIRU;
+// if(true){
+//     console.log(warnaBaju);
+// }
+// const stringIsiKutipSatu = 'ini string tapi aku bisa nulis kutip dua disini, liat nih ", tuh kan !'
+// console.log(stringIsiKutipSatu);
+// const namaDepan = "Zaki";
+// const tempatLahir = "Tenggarong";
+// console.log(`Hallo, Nama ku ${namaDepan}, aku lahir di ${tempatLahir}`);
+// const hewan = ['zebra','macan','kudanil']
+
+// const hewanZebra = (arrHewan) => arrHewan.reduce((total, current) => {
+//     if(current === 'zebra'){
+//         total.nama =current
+//     }
+//     return total
+// }, {nama : ''})
+
+// console.log(hewanZebra(hewan))
+
+const newHewan = Object.assign({}.hewan)
+console.log(newHewan)
--- tugasFibonacci.js
+++ tugasFibonacci.js
@@ -21,38 +21,47 @@ const bilanganFibonacci2 = n =>{
     let fib = 1
     let fib_1 = 1
     let fib_2 = 0
-    if(n < 8 && n > 5){
-        for(let i = 0; i<5; i++){
+    for(let i = 0; i<n; i++){
+        if(fib <= n){
             arr.push(fib)
             fib = fib_1 + fib_2
             fib_2 = fib_1
             fib_1 = fib
         }
-        return arr
-    }else if(n < 13 && n >= 8){
-        for(let i = 0; i<6; i++){
-            arr.push(fib)
-            fib = fib_1 + fib_2
-            fib_2 = fib_1
-            fib_1 = fib
-        }
-        return arr
-    }else if(n < 21 && n >= 13){
-        for(let i = 0; i<7; i++){
-            arr.push(fib)
-            fib = fib_1 + fib_2
-            fib_2 = fib_1
-            fib_1 = fib
-        }
-        return arr
-    }else{
-        for(let i = 0; i<n; i++){
-            arr.push(fib)
-            fib = fib_1 + fib_2
-            fib_2 = fib_1
-            fib_1 = fib
-        }
-        return arr
     }
+    // if(n < 8 && n > 5){
+    //     for(let i = 0; i<5; i++){
+    //         arr.push(fib)
+    //         fib = fib_1 + fib_2
+    //         fib_2 = fib_1
+    //         fib_1 = fib
+    //     }
+    //     return arr
+    // }else if(n < 13 && n >= 8){
+    //     for(let i = 0; i<6; i++){
+    //         arr.push(fib)
+    //         fib = fib_1 + fib_2
+    //         fib_2 = fib_1
+    //         fib_1 = fib
+    //     }
+    //     return arr
+    // }else if(n < 21 && n >= 13){
+    //     for(let i = 0; i<7; i++){
+    //         arr.push(fib)
+    //         fib = fib_1 + fib_2
+    //         fib_2 = fib_1
+    //         fib_1 = fib
+    //     }
+    //     return arr
+    // }else{
+    //     for(let i = 0; i<n; i++){
+    //         arr.push(fib)
+    //         fib = fib_1 + fib_2
+    //         fib_2 = fib_1
+    //         fib_1 = fib
+    //     }
+    //     return arr
+    // }
+    return arr
 }
-console.log(bilanganFibonacci2(6))
+console.log(bilanganFibonacci2(10))
