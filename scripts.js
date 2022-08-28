63d0d886dcc06c000a5e42db1b778f070637cab0
Muhammad Zaki Kurniawan <kurniawan.zaki279@gmail.com>
2022-08-28 21:36:59 +0700

    add file text.txt and add module fs try to read and write with module fs

--- scripts.js
+++ scripts.js
@@ -1,54 +1,65 @@
-// belajar var
-var diskon,nama;
-nama = "aku";
-nama = "siapa?"
-diskon = 500;
-if(diskon == true){
-    diskon = 300;
-}
-console.log(nama);
-console.log(diskon); //outputnya 500 karena variabel diskon bersifat global
+//belajar module pada node
+// const os = require('os')
+// console.log('free memory:', os.freemem())
+// const nilaiKelulusan = require('./tugasNilaiNaikKelas.js')
+// console.log(nilaiKelulusan(90,80,75,80))
+// const fs = require('fs')
+// const isi = fs.writeFileSync('./text.txt',"Aku Cinta Kamu")
+// console.log(isi)
+const fs = require('fs')
+const biodata = require('./person.json')
+console.log(biodata)
+// // belajar var
+// var diskon,nama;
+// nama = "aku";
+// nama = "siapa?"
+// diskon = 500;
+// if(diskon == true){
+//     diskon = 300;
+// }
+// console.log(nama);
+// console.log(diskon); //outputnya 500 karena variabel diskon bersifat global
 
-function scopeDiskon(){
-    var diskon = 300;
-    console.log(diskon)// outputnya 300 karena variabel diskon bersifat lokal
-}
-scopeDiskon()
-// end belajar var
-// button
-function myFunction(){
-    document.getElementById("ubah").innerHTML ="aku zaki";
-}
-// end button
-// belajar let
-let potongan;
-potongan = 100;
-if(true){
-    potongan = 400;
-    console.log(potongan); //outputnya 400 karena let diskon bersifat lokal
-}
-// end belajar let
-// belajar const
-const WARNA_MERAH = "#F00";
-const WARNA_BIRU = "#00F";
-const WARNA_HIJAU = "#0F0";
+// function scopeDiskon(){
+//     var diskon = 300;
+//     console.log(diskon)// outputnya 300 karena variabel diskon bersifat lokal
+// }
+// scopeDiskon()
+// // end belajar var
+// // button
+// function myFunction(){
+//     document.getElementById("ubah").innerHTML ="aku zaki";
+// }
+// // end button
+// // belajar let
+// let potongan;
+// potongan = 100;
+// if(true){
+//     potongan = 400;
+//     console.log(potongan); //outputnya 400 karena let diskon bersifat lokal
+// }
+// // end belajar let
+// // belajar const
+// const WARNA_MERAH = "#F00";
+// const WARNA_BIRU = "#00F";
+// const WARNA_HIJAU = "#0F0";
 
-let warnaBaju = WARNA_BIRU;
-if(true){
-    console.log(warnaBaju);
-}
-const stringIsiKutipSatu = 'ini string tapi aku bisa nulis kutip dua disini, liat nih ", tuh kan !'
-console.log(stringIsiKutipSatu);
-const namaDepan = "Zaki";
-const tempatLahir = "Tenggarong";
-console.log(`Hallo, Nama ku ${namaDepan}, aku lahir di ${tempatLahir}`);
-const hewan = ['zebra','macan','kudanil']
+// let warnaBaju = WARNA_BIRU;
+// if(true){
+//     console.log(warnaBaju);
+// }
+// const stringIsiKutipSatu = 'ini string tapi aku bisa nulis kutip dua disini, liat nih ", tuh kan !'
+// console.log(stringIsiKutipSatu);
+// const namaDepan = "Zaki";
+// const tempatLahir = "Tenggarong";
+// console.log(`Hallo, Nama ku ${namaDepan}, aku lahir di ${tempatLahir}`);
+// const hewan = ['zebra','macan','kudanil']
 
-const hewanZebra = (arrHewan) => arrHewan.reduce((total, current) => {
-    if(current === 'zebra'){
-        total.nama =current
-    }
-    return total
-}, {nama : ''})
+// const hewanZebra = (arrHewan) => arrHewan.reduce((total, current) => {
+//     if(current === 'zebra'){
+//         total.nama =current
+//     }
+//     return total
+// }, {nama : ''})
 
-console.log(hewanZebra(hewan))
+// console.log(hewanZebra(hewan))
--- tugasNilaiNaikKelas.js
+++ tugasNilaiNaikKelas.js
@@ -1,9 +1,20 @@
-const nilaiKelulusan = (a,b,c,absen) =>{
-    nilaiTotal = (a+b+c)/3
-    if(nilaiTotal&absen >= 80 ){
-        return 'Naik Kelas'
-    }else{
-        return 'Tidak Naik Kelas'
-    }
+// const nilaiKelulusan = (a,b,c,absen) =>{
+//     nilaiTotal = (a+b+c)/3
+//     if(nilaiTotal&absen >= 80 ){
+//         return 'Naik Kelas'
+//     }else{
+//         return 'Tidak Naik Kelas'
+//     }
+// }
+// module.exports = nilaiKelulusan
+// // console.log(nilaiKelulusan(90,70,75,80))
+
+const createPerson = function(person){
+    fs.writeFileSync('./person.json',JSON.stringify(person))
+    return person
 }
-console.log(nilaiKelulusan(90,70,75,80))
+const biodata = createPerson({
+    nama: 'zaki',
+    umur: 21,
+    alamat: 'Malang'
+})
